2680fe28c4fa010483c2a2446cc7f3bab2201afc
Elizeu Oliveira da Silva <elizeu_os@hotmail.com>
2019-10-20 11:51:44 -0300

    Installs wordpress

--- index.php
+++ index.php
@@ -1,62 +1,17 @@
-<?php include('partials/header.php') ?>
+<?php
+/**
+ * Front to the WordPress application. This file doesn't do anything, but loads
+ * wp-blog-header.php which does and tells WordPress to load the theme.
+ *
+ * @package WordPress
+ */
 
-    <section aria-labeledby="mainTitle" class="c-section c-section--main-section">
-        <div class="c-container c__center">
+/**
+ * Tells WordPress to load the WordPress theme and output it.
+ *
+ * @var bool
+ */
+define( 'WP_USE_THEMES', true );
 
-            <h1 id="mainTitle" class="c__title c__white-text c-title--main-title c__center">
-            <span class="c__section-title__big-title">
-                Reverbera
-            </span>
-                <br>O conhecimento através dos seus ouvidos
-            </h1>
-        </div>
-    </section>
-
-    <section class="c-section">
-        <div class="c-container c__center">
-            <div class="l-flex l-flex--container">
-                <?php include('partials/side-menu.php') ?>
-                <main id="conteudo" accesskey="1" class="c-content-wrapper">
-
-                    <div class="c-breadcrumb">
-                        <div class="l-flex l-flex--center">
-                            <div id="breadcrumb" class="c-bread-crumb__description">
-                                Você está aqui:
-                            </div>
-                            <ol role="navigation" class="l-flex">
-                                <li class="c-breadcrumb__link">
-                                    <a href="#" aria-current="page" class="c__link c__trasition300">
-                                        Página inicial
-                                    </a>
-                                </li>
-                            </ol>
-                        </div>
-                    </div>
-
-                    <h2 class="c__title c__center">
-                        Nossa missão
-                    </h2>
-                    <p class="c__paragraph">
-                        A Reverbera surgiu com a necessidade de ampliar os meios de acesso à audiolivros
-                        online de maneira gratuita e acessível. Contribuindo com a reverberação do conhecimento e
-                        diminuição
-                        das barreiras digitais.
-                    </p>
-                    <h2 class="c__title c__center">
-                        Últimos lançamentos
-                    </h2>
-                    <ul class="l-flex l-flex--center l-flex__wrap">
-                        <?php
-                        include('partials/audiobook-item.php');
-                        include('partials/audiobook-item.php');
-                        include('partials/audiobook-item.php');
-                        include('partials/audiobook-item.php');
-                        include('partials/audiobook-item.php');
-                        include('partials/audiobook-item.php');
-                        ?>
-                    </ul>
-                </main>
-            </div>
-        </div>
-    </section>
-<?php include('partials/footer.php') ?>
+/** Loads the WordPress Environment and Template */
+require( dirname( __FILE__ ) . '/wp-blog-header.php' );
