21b72a7ff794c6334624560933fff26ddded50cd
Elizeu Oliveira da Silva <elizeu_os@hotmail.com>
2019-10-26 16:10:34 -0300

    Creates the custom post type and taxonomy

--- wp-content/themes/reverbera/functions.php
+++ wp-content/themes/reverbera/functions.php
@@ -42,3 +42,53 @@ function registerStyles()
 }
 
 add_action('wp_enqueue_scripts', 'registerStyles');
+
+function addCustomPostTypeAudiolivro() {
+    $labels = array(
+        'name' => _x('Áudiolivros', 'post type general name'),
+        'singular_name' => _x('Áudiolivro', 'post type singular name'),
+        'add_new' => _x('Adicionar Novo', 'Novo item'),
+        'add_new_item' => __('Novo Item'),
+        'edit_item' => __('Editar Item'),
+        'new_item' => __('Novo Item'),
+        'view_item' => __('Ver Item'),
+        'search_items' => __('Procurar Itens'),
+        'not_found' =>  __('Nenhum registro encontrado'),
+        'not_found_in_trash' => __('Nenhum registro encontrado na lixeira'),
+        'parent_item_colon' => '',
+        'menu_name' => 'Áudiolivros',
+
+    );
+
+    $args = array(
+        'labels' => $labels,
+        'public' => true,
+        'public_queryable' => true,
+        'show_ui' => true,
+        'query_var' => true,
+        'rewrite' => true,
+        'capability_type' => 'post',
+        'has_archive' => true,
+        'hierarchical' => false,
+        'menu_position' => null,
+        'menu_icon'   => 'dashicons-format-audio',
+        'supports' => array('title','editor','thumbnail','custom-fields')
+    );
+
+    register_post_type( 'audiolivros' , $args );
+    flush_rewrite_rules();
+}
+
+add_action('init', 'addCustomPostTypeAudiolivro');
+
+register_taxonomy(
+    "categorias",
+    "audiolivros",
+    array(
+        "label" => "Categorias",
+        "singular_label" => "Categoria",
+        "rewrite" => true,
+        "hierarchical" => true,
+        'menu_icon'   => 'dashicons-tag'
+    )
+);
